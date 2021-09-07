0923d8cd816a95bfe2d018b60eb7eac4ca5b9192
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-07 12:45:22 -0500

    Fix exercise 1, tabs to spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,11 @@ number = random.randint(-10000, 10000)
 last = int(repr(number)[-1])
 
 if last > 5 and number > 0:
-	print("Last digit of", number, "is", last, "and is greater than 5")
+    print("Last digit of", number, "is", last, "and is greater than 5")
 elif last == 0:
-	print("Last digit of", number, "is", last, "and is 0")
+    print("Last digit of", number, "is", last, "and is 0")
 else:
-	if number < 0:
-		print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last))
-	else:
-		print("Last digit of", number, "is", last, "and is less than 6 and not 0")
+    if number < 0:
+        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last))
+    else:
+        print("Last digit of", number, "is", last, "and is less than 6 and not 0")
