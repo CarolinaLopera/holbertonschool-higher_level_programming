e8e6949263b0dc7c18b63b2d419484a97f240694
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-07 13:51:17 -0500

    Fix exercise 1, change last

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,20 @@
 import random
 number = random.randint(-10000, 10000)
 
-last = int(repr(number)[-1])
-
-if last > 5 and number > 0:
-    print("Last digit of", number, "is", last, "and is greater than 5")
-elif last == 0:
-    print("Last digit of", number, "is", last, "and is 0")
-elif (last < 6 and last != 0) or number < 0:
-    if number < 0:
-        print("Last digit of {} is ".format(number), end="")
-        print("-{} and is less than 6 and not 0".format(last))
+if number >= 0:
+    last = number % 10
+    if last > 5:
+        print("Last digit of", number, "is", last, "and is greater than 5")
+    elif last == 0:
+        print("Last digit of", number, "is", last, "and is 0")
     else:
         print("Last digit of", number, "is ", end="")
         print(last, "and is less than 6 and not 0")
+
+if number < 0:
+    last = (number * -1) % 10
+    if last == 0:
+        print("Last digit of", number, "is", last, "and is 0")
+    else:
+        print("Last digit of {} is ".format(number), end="")
+        print("-{} and is less than 6 and not 0".format(last))
