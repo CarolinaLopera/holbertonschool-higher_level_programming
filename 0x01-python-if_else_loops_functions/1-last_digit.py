e24a46fdc38caa64a59285c7f051df26d3615fff
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-07 12:05:30 -0500

    Exercise of the 0 to 3

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last = int(repr(number)[-1])
+
+if last > 5 and number > 0:
+	print("Last digit of", number, "is", last, "and is greater than 5")
+elif last == 0:
+	print("Last digit of", number, "is", last, "and is 0")
+else:
+	if number < 0:
+		print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last))
+	else:
+		print("Last digit of", number, "is", last, "and is less than 6 and not 0")
