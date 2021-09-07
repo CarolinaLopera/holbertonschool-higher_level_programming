cee2fb8b9466db0ad9fb2fbe3cacac4a59d39de9
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-07 13:04:25 -0500

    Checkers fail, return elif to else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if last > 5 and number > 0:
     print("Last digit of", number, "is", last, "and is greater than 5")
 elif last == 0:
     print("Last digit of", number, "is", last, "and is 0")
-elif (last < 6 and last != 0) or number < 0:
+else:
     if number < 0:
         print("Last digit of {} is ".format(number), end="")
         print("-{} and is less than 6 and not 0".format(last))
