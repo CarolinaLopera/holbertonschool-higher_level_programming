9b0b4ab595c0f69a43a13fa82b3a72ad73a033b3
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-08 15:12:19 -0500

    Print arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv, exit
+    from sys import argv
 
 num_args = len(argv) - 1
 
 if num_args == 0:
     print("0 arguments.")
-    exit()
 elif num_args == 1:
     print("1 argument:")
 else:
