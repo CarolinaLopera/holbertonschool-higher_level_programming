74a14bf02885dd9cf7a3c0801c248bb20fd86ffd
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-08 00:12:27 -0500

    print alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+flag = 1
 for i in range(122, 96, -1):
+    aux = i
+    if flag == 0:
+        i -= 32
+        flag = 1
+    elif flag == 1:
+        flag = 0
+
     print("{}".format(chr(i)), end="")
+    i = aux
