5afa96029d524023c2e35098088b3cbc26e945ad
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-08 23:12:39 -0500

    node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -22,9 +22,9 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (head == NULL)
 	{
-		*head = new;
-		new->next = NULL;
+		new->next = head_cpy;
 		new->n = number;
+		(*head) = new;
 	}
 
 	while (head != NULL)
