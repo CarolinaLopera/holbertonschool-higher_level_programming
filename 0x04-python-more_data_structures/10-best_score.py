eae7b2611b42190d1837754939fd45e5eacf5504
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-16 00:16:56 -0500

    Biggest

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if not a_dictionary:
         return None
     order = sorted(a_dictionary.items(), key=lambda x: x[1])
     return order[-1][0]
