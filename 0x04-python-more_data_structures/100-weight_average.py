f91d7b929fc32549ee5512ad18a0da0ee40fbf7a
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-18 10:50:14 -0500

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if list is None or len(my_list) == 0:
+        return 0
+    mul = 0
+    div = 0
+    for i in my_list:
+        mul += i[0] * i[1]
+        div += i[1]
+
+    return mul / div
