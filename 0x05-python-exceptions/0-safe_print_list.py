ced19bba1fd4be6c2e165f8946fbd75c1abc8e48
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-27 12:05:25 -0500

    Try except until x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
         except IndexError:
-            print()
-            return i
+            i -= 1
+            break
     print()
     return i + 1
+
