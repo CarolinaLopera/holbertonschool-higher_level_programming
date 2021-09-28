7600c22d8aecdfdfbede84f50b3c312e5f41fb17
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-27 22:51:20 -0500

    Pointer to function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,9 +3,9 @@ import sys
 
 
 def safe_function(fct, *args):
-    result = None
     try:
         result = fct(*args)
     except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        result = None
     return result
