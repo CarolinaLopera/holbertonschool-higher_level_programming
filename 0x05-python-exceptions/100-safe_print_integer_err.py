88e11f812f9e9c883ac047eeb36a7d7cab1d0f90
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-27 22:44:02 -0500

    Pointer to function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError):
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         # sys.stderr.write("Exception: ")
         # sys.stderr.write("Unknown format code 'd' for object of type 'str'")
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+        return result
+    except Exception:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
