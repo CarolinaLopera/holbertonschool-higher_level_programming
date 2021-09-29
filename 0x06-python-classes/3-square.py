898c09be2e677d5c4de489fcec18b8e6d08f7cbe
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-28 23:13:56 -0500

    Square Area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Class to Square'''
+
+
+class Square:
+    '''This class have a size'''
+
+    def __init__(self, size=0):
+        '''This class raise error'''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''This method computes the area'''
+        return self.__size * self.__size
