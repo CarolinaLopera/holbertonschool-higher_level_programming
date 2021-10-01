1954273a5c48b700fead3ec85b69505fc6990987
CarolinaLopera <carolinalopera08@gmail.com>
2021-09-30 19:02:27 -0500

    Fix rectangle of size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -42,6 +42,21 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
 
+    @classmethod
+    def square(cls, size=0):
+        '''Returns a new instance of Rectangle'''
+        return cls(size, size)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if isinstance(rect_1, Rectangle) is False:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif isinstance(rect_2, Rectangle) is False:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
     def area(self):
         '''This method computes the area'''
         return self.__height * self.__width
@@ -71,18 +86,3 @@ class Rectangle:
     def __del__(self):
         print("Bye rectangle...")
         type(self).number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if isinstance(rect_1, Rectangle) is False:
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        elif isinstance(rect_2, Rectangle) is False:
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        '''Returns a new instance of Rectangle'''
-        return cls(size, size)
