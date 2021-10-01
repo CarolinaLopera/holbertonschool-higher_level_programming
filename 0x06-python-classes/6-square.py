d1bf9064c8716d6e92e3bc38ac67ff5b85c636aa
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-01 10:17:46 -0500

    Fix integers positive

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,9 +35,9 @@ class Square:
         self.__position = value
         if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[0]) is not int:
+        elif type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif type(value[1]) is not int:
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
