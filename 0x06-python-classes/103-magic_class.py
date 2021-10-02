2e1d9cd3df163649a7be9e9c0be6945af9913b92
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-02 00:40:18 -0500

    Magic Class bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -18,4 +18,4 @@ class MagicClass:
 
     def circumference(self):
         '''This method computes the circumference'''
-        return 2 * math.pi * self.__radius
+        return (2 * math.pi * self.__radius)
