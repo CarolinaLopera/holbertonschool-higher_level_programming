c70bb453bbff30b5a013cd5783cf2a0fa34e9e9d
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-03 00:04:58 -0500

    Fix posicion empty and \n

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,14 +52,11 @@ class Square:
         if self.square == 0:
             print()
             return
-        # [print("") for i in range(0, self.__position[1])]
         for j in range(0, self.__position[1]):
             print("")
         for i in range(self.square):
             for j in range(0, self.__position[0]):
                 print(" ", end="")
-                # else:
-                #     print(" ", end="")
             for i in range(self.square):
                 print("#", end="")
             print()
