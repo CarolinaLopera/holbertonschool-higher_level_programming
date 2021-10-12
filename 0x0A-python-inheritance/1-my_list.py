3616b90a127f7b0e7b2343c0ea331f8c3dab4976
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-11 21:42:53 -0500

    list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''This class sorted a list
+and later it is print'''
+
+
+class MyList(list):
+    '''list is the list to print'''
+
+    def print_sorted(self):
+        '''self is list'''
+        sort = sorted(self)
+        print(sort)
