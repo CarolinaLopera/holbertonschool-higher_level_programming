c7d108cf60c6473eae6cc69cafc25b7391f557b9
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-12 19:29:08 -0500

    Exercise 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''This function read a file
+and it is write in the stdout'''
+
+
+def read_file(filename=""):
+    '''filename is de name of the file'''
+    with open(filename) as f:
+        read_file = f.read()
+        print(read_file)
