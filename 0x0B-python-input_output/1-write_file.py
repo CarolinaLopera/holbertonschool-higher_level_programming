04059af61d1786d4e6be670067169754967b6150
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-12 19:58:20 -0500

    Exercise 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@ Return the number of characteres write'''
 
 
 def write_file(filename="", text=""):
-    with open(filename, 'w') as f:
+    with open(filename, 'w+') as f:
         write_file = f.write(text)
         return write_file
