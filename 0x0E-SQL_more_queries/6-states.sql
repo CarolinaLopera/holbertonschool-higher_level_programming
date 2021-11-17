3d0d1b991071825b63ae63b78e10933e901bffa5
CarolinaLopera <carolinalopera08@gmail.com>
2021-11-17 17:01:28 -0500

    Exercise 6, fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- This script creates a database and a table.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
