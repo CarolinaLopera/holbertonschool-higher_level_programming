a97d9175d22c787e861954b7fa1e221dc5e04eb8
CarolinaLopera <carolinalopera08@gmail.com>
2021-11-17 14:01:35 -0500

    Add comment

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- This script creates a database and the user.
+-- Give permisions to select.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
