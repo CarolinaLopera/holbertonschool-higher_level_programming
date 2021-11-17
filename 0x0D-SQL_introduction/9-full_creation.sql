dda5f24886f5a09f2395d5d72552663cda8b6c04
CarolinaLopera <carolinalopera08@gmail.com>
2021-11-16 23:44:22 -0500

    10

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
---
+-- Thisscript that creates a table in a database
+-- and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
     id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES (1, 'John', 10);
