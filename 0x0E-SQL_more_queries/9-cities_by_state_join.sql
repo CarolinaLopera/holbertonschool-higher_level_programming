018882ba2e59835091d11d24b0749e14c4d223da
CarolinaLopera <carolinalopera08@gmail.com>
2021-11-17 20:44:58 -0500

    inner join, 9, AS

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- This script lists all cities contained in a database.
 SELECT cities.id, cities.name, states.name
-    FROM cities c
-    INNER JOIN states s
+    FROM cities AS c
+    INNER JOIN states AS s
     ON c.id=s.id
 ORDER BY c.id;
