ad6a53784a08a034b749db0fd6a8d377ff1b0ea6
CarolinaLopera <carolinalopera08@gmail.com>
2021-12-13 19:30:12 -0500

    6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+let i = 0;
+
+for (; i < 3; i++) {
+  switch (i) {
+    case 0:
+      console.log('C is fun');
+      break;
+    case 1:
+      console.log('Python is cool');
+      break;
+    case 2:
+      console.log('JavaScript is amazing');
+      break;
+  }
+}
