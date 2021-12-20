23dac7734a7a01f9c084a67e31b6fa58349ff6c4
Carolina Lopera <carolinalopera08@gmail.com>
2021-12-20 19:38:08 +0000

    3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''Lists states from a database'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         password=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    result = cur.fetchall()
+    for i in result:
+        if i[1] == argv[4]:
+            print(i)
