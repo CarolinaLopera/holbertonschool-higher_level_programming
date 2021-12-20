1e0776d24a9a535b6fd1f50871542729c4c26325
Carolina Lopera <carolinalopera08@gmail.com>
2021-12-20 19:17:46 +0000

    1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+  
+'''Lists all states from a database'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         password=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    for i in cur.fetchall():
+        if i[1][0] == 'N':
+            print(i)
+
+    db.close()
