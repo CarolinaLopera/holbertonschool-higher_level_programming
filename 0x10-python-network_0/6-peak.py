539eb8f86a38e93872bd19594a172a945e63eb17
CarolinaLopera <carolinalopera08@gmail.com>
2022-01-13 11:28:03 -0500

    Add validation of type

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,8 @@
 
 def find_peak(list_of_integers):
     '''Order a list and return the greater number'''
+    if type(list_of_integers) != list:
+        return None
     if len(list_of_integers) == 0:
         return None
     return max(list_of_integers)
