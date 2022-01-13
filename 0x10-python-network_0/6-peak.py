44edefc1a2112afc7b1af8bacdb8d221f3aba568
CarolinaLopera <carolinalopera08@gmail.com>
2022-01-12 20:46:27 -0500

    Fix documentacion 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''This function order a list and return the greater number'''
+'''This is my function'''
 
 
 def find_peak(list_of_integers):
+    '''Order a list and return the greater number'''
     if len(list_of_integers) == 0:
         return None
     order_list = sorted(list_of_integers)
